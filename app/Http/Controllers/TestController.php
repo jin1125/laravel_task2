d9cf6da340f529ce088dbe45febcf97c0c10f4f6
jin1125 <shogo.jinta@gmail.com>
2022-04-17 18:10:22 +0900

    86

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use  App\Models\Test;
+use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
@@ -11,7 +12,11 @@ class TestController extends Controller
     {
         $values = Test::all();
 
-        // dd($values);
+        $tests = DB::table('tests')
+        ->select('id')
+        ->get();
+
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
