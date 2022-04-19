fc030179d21301c865fa9930a1de0a32d5c2ac26
jin1125 <shogo.jinta@gmail.com>
2022-04-19 09:55:58 +0900

    108

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\ContactForm;
 use Illuminate\Support\Facades\DB;
+use App\Services\CheckFormData;
 
 class ContactFormController extends Controller
 {
@@ -69,7 +70,11 @@ class ContactFormController extends Controller
     {
         $contact = ContactForm::find($id);
 
-        return view('contact.show', compact('contact'));
+        $gender = CheckFormData::checkGender($contact);
+
+        $age = CheckFormData::checkAge($contact);
+
+        return view('contact.show', compact('contact', 'gender', 'age'));
     }
 
     /**
