55cc701ba6893923770eb9add54ea75247e41928
jin1125 <shogo.jinta@gmail.com>
2022-04-19 11:51:34 +0900

    111

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => Str::random(10),
+                'email' => 'shogo.jinta'.'@gmail.com',
+                'password' => Hash::make('password123'),
+            ],[
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password123'),
+            ],[
+                'name' => Str::random(10),
+                'email' => Str::random(10).'@gmail.com',
+                'password' => Hash::make('password123'),
+            ],
+        ]);
+    }
+}
